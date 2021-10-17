3d2237220071c77ec536788ff23b881d9cb27a58
unknown <rodionsharlov@gmail.com>
2021-10-17 12:21:43 +0800

    Add 977. Squares of a Sorted Array solution

---
+++ 977. Squares of a Sorted Array/Solution.kt
@@ -0,0 +1,42 @@
+fun sortedSquares(nums: IntArray): IntArray {
+    var output = IntArray(0)
+    var negMults = IntArray(0)
+    var negInd = -1
+
+    for (num in nums) {
+        val mult = num * num
+        if (num < 0 ) {
+            negMults += mult
+            negInd = negMults.lastIndex
+        } else {
+            if (negInd > -1) {
+                while (negInd > -1) {
+                    if (mult >= negMults[negInd]) {
+                        output += negMults[negInd]
+                        negInd--
+                    } else break
+                }
+            }
+            output += mult
+        }
+    }
+    if (negInd > -1) {
+        for (ind in negInd downTo 0) {
+            output += negMults[ind]
+        }
+    }
+    return output
+}
+
+fun main () {
+    var nums: IntArray
+    nums = intArrayOf(-7,-3,2,3,11)
+    // > 4 9 9 49 121
+    nums = intArrayOf(-4,-1,0,3,10)
+    // > 0 1 9 16 100
+    nums = intArrayOf(-11, 10)
+    // > 100 121
+
+    println(nums.joinToString(" "))
+    println(sortedSquares(nums).joinToString(" "))
+}
